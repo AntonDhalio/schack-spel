ab7ab8582b9ff1e057d87f150827718e3f31726f
AntonDhalio <83759822+AntonDhalio@users.noreply.github.com>
2022-05-23 21:31:15 +0200

    Update Board.js

--- my-app/src/Components/Board.js
+++ my-app/src/Components/Board.js
@@ -3,24 +3,11 @@ import './Board.css'
 
 const Board = () => {
   return (
-    <table className='no-border'>
-      <thead>
-        <tr className='upside-down'><th></th><th>H</th><th>G</th><th>F</th><th>E</th><th>D</th><th>C</th><th>B</th><th>A</th><th></th></tr>
-      </thead>
-      <tbody>
-        <tr><th>8</th><td className='square-light'></td><td className='square-dark'></td><td className='square-light'></td><td className='square-dark'></td><td className='square-light'></td><td className='square-dark'></td><td className='square-light'></td><td className='square-dark'></td><th className='upside-down'>8</th></tr>
-        <tr><th>7</th><td className='square-dark'></td><td className='square-light'></td><td className='square-dark'></td><td className='square-light'></td><td className='square-dark'></td><td className='square-light'></td><td className='square-dark'></td><td className='square-light'></td><th className='upside-down'>7</th></tr>
-        <tr><th>6</th><td className='square-light'></td><td className='square-dark'></td><td className='square-light'></td><td className='square-dark'></td><td className='square-light'></td><td className='square-dark'></td><td className='square-light'></td><td className='square-dark'></td><th className='upside-down'>6</th></tr>
-        <tr><th>5</th><td className='square-dark'></td><td className='square-light'></td><td className='square-dark'></td><td className='square-light'></td><td className='square-dark'></td><td className='square-light'></td><td className='square-dark'></td><td className='square-light'></td><th className='upside-down'>5</th></tr>
-        <tr><th>4</th><td className='square-light'></td><td className='square-dark'></td><td className='square-light'></td><td className='square-dark'></td><td className='square-light'></td><td className='square-dark'></td><td className='square-light'></td><td className='square-dark'></td><th className='upside-down'>4</th></tr>
-        <tr><th>3</th><td className='square-dark'></td><td className='square-light'></td><td className='square-dark'></td><td className='square-light'></td><td className='square-dark'></td><td className='square-light'></td><td className='square-dark'></td><td className='square-light'></td><th className='upside-down'>3</th></tr>
-        <tr><th>2</th><td className='square-light'></td><td className='square-dark'></td><td className='square-light'></td><td className='square-dark'></td><td className='square-light'></td><td className='square-dark'></td><td className='square-light'></td><td className='square-dark'></td><th className='upside-down'>2</th></tr>
-        <tr><th>1</th><td className='square-dark'></td><td className='square-light'></td><td className='square-dark'></td><td className='square-light'></td><td className='square-dark'></td><td className='square-light'></td><td className='square-dark'></td><td className='square-light'></td><th className='upside-down'>1</th></tr>
-      </tbody>
-      <tfoot>
-        <tr><th></th><th>A</th><th>B</th><th>C</th><th>D</th><th>E</th><th>F</th><th>G</th><th>H</th><th></th></tr>
-      </tfoot>
-    </table>
+    <Grid container spacing={0}>
+      <Grid item xs={1}>
+        <item>1A</item>
+      </Grid>
+    </Grid>
   )
 }
 
